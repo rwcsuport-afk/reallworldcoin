07087bbfbe0b537d8b4257bbb685840f2f39551c
rwcsuport-afk <rwcsuport@gmail.com>
2025-08-27 12:52:36 +0530

    new_packeage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -39,7 +39,6 @@
 
     <!-- Custom - CSS Include -->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .hero_title {
