ec28a5d4dd8a3f5224fafac6da85c6cecaf89ecc
rwcsuport-afk <rwcsuport@gmail.com>
2025-08-27 14:40:09 +0530

    new_npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,18 @@
 const mix = require('laravel-mix');
+
 mix.js('resources/js/app.js', 'public/js')
-    // .sass('resources/sass/app.scss', 'public/css') // remove/comment if not needed
     .version();
+
+mix.webpackConfig({
+    resolve: {
+        fallback: {
+            fs: false,
+            path: false,
+            crypto: false,
+            stream: false,
+        },
+    },
+    experiments: {
+        topLevelAwait: true, // Enables modern ESM syntax like `await` at top-level
+    },
+});
