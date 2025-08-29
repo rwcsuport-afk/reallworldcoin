a0143819c480b612417d176669ee1196dbccc94b
rwcsuport-afk <rwcsuport@gmail.com>
2025-08-29 09:54:09 +0530

    new

--- webpack.mix.js
+++
@@ -1,6 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js') // ✅ keep app.js
-    .js('resources/js/wallet.js', 'public/js') // ✅ add wallet.js
-    .postCss('resources/css/app.css', 'public/css', [])
-    .version();
