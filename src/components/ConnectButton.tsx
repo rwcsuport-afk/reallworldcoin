43d1cd162e58d9130cd4a3f59cf4a33176b6b4a4
rwcsuport-afk <rwcsuport@gmail.com>
2025-08-29 11:38:04 +0530

    all

--- src/components/ConnectButton.tsx
+++
@@ -1,5 +0,0 @@
-export default function ConnectButton() {
-// AppKit exposes a global web component to trigger the modal
-// No import needed; just use the custom element.
-return <appkit-button />
-}
