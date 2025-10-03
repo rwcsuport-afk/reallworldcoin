82750607bae95200990a9833dffe76723652c06b
rwcsuport-afk <rrwcsuport@gmail.com>
2025-10-03 13:13:21 +0530

    composer

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ define('LARAVEL_START', microtime(true));
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 */
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -19,14 +19,14 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 | Register The Auto Loader
 |--------------------------------------------------------------------------
 */
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
